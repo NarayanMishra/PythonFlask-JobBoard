1efa780acd1bb3c8e1e3503602af947c62d59b7a
NarayanMishra <38766761+NarayanMishra@users.noreply.github.com>
2021-08-11 12:21:41 -0700

    Update app.py
    Close the Connection Decorator

--- jobs/app.py
+++ jobs/app.py
@@ -20,12 +20,13 @@ def execute_sql(sql, values=(), commit=False, single=False):
     cursor.close()
     return results
 
+app = Flask(__name__)
+@app.teardown_appcontext
 def close_connection(exception):
     connection = getattr(g, '_connection', None)
     if connection is not None:
         connection.close()
         
-app = Flask(__name__)
 @app.route('/')
 @app.route('/jobs')
 
