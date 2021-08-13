24ca4af4cbbc2f2356d817a475873b2baa7e7e83
NarayanMishra <38766761+NarayanMishra@users.noreply.github.com>
2021-08-12 18:24:33 -0700

    Update app.py

--- jobs/app.py
+++ jobs/app.py
@@ -1,5 +1,7 @@
 import sqlite3
-from flask import Flask, render_template, g
+import datetime
+from flask import Flask, render_template, g, request, redirect, url_for
+
 PATH = 'db/jobs.sqlite'
 
 def open_connection():
